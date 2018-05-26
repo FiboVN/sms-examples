ec6df1104d67766a2a03e4d1313a47b2358d4d83
Luc <luc@ltv.vn>
2018-05-26 15:52:59 +0700

    Add lib/sms.php

--- php/lib/sms.php
+++ php/lib/sms.php
@@ -1,16 +1,27 @@
 <?php 
 class SMS { 
-    private $endpoint = 'http://center.fibosms.com/service.asmx'; 
+    private $endpoint = 'http://center.fibosms.com/service.asmx?wsdl'; 
     private $clientNo = '';
     private $clientPass = '';
-    
+    private $soapclient = NULL;
     
     function __construct($clientNo, $clientPass) {
       $this->clientNo = $clientNo;
       $this->clientPass = $clientPass;
+      $this->soapclient = new SoapClient($this->endpoint);
     }
 
     function sendSMS($msg) {
-      return false;
+      $params = array(
+        'clientNo' => $this->clientNo,
+        'clientPass' => $this->clientPass,
+        'senderName' => $msg['senderName'],
+        'phoneNumber' => $msg['phoneNumber'],
+        'smsMessage' => $msg['smsMessage'],
+        'smsGUID' => $msg['smsGUID'],
+        'serviceType' => 0
+      );
+      print_r($params);
+      return $this->soapclient->SendMaskedSMS($params);;
     }
 } 
